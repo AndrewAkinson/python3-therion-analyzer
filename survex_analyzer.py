d73db56b3f9e8cf9ed9e42106d79f08936158eaa
Patrick Warren <patrickbwarren@gmail.com>
2023-10-11 09:59:24 +0100

    added example data

--- survex_analyzer.py
+++ survex_analyzer.py
@@ -28,19 +28,17 @@ along with this program.  If not, see
 import pandas as pd
 from pathlib import Path
 
-def open_with_character_encoding(p, trace=False):
-    '''Return a file pointer after figuring out the character encoding for a file'''
-    for encoding in ['UTF-8', 'iso-8859-1', 'ascii']: # list of options to try
+def character_encoding(p):
+    '''Try to figure out the character encoding that works for a file'''
+    for encoding in ['utf-8', 'iso-8859-1', 'ascii']: # list of options to try
         with p.open('r', encoding=encoding) as fp:
             try:
-                lines = fp.readlines()
+                fp.readlines() # we don't need to capture the output here
             except UnicodeDecodeError:
                 pass
-            else: # if we didn't fail, we found a possible encoding
+            else: # if we didn't fail, we found something that works
                 break
-    if trace:
-        print(f'Analyzing {p} ({encoding})')
-    return p.open('r', encoding=encoding), encoding # return the file pointer AND the encoding
+    return encoding
 
 def extract_star_command(clean, star_commands):
     '''Extract a star command from a cleaned up line, returning None if not present'''
@@ -55,8 +53,7 @@ class Analyzer:
     
     def __init__(self,
                  star_commands=['*include', '*begin', '*end', '*fix', '*equate', '*cs', '*cs out', '*export', '*date', '*flags'],
-                 # schema={'directory':str, 'file':str, 'line':int, 'survex_path':str, 'COMMAND':str, 'argument':str, 'full':str},
-                 schema={'file':str, 'line':int, 'survex_path':str, 'COMMAND':str, 'argument':str, 'full':str},
+                 schema={'file':str, 'encoding':str, 'line':int, 'survex_path':str, 'COMMAND':str, 'argument':str, 'full':str},
                  comment_char=';'):
         self.star_commands = star_commands
         self.comment_char = comment_char
@@ -68,27 +65,31 @@ class Analyzer:
     # iteration.
 
     def analyze(self, svx_file, trace=False, absolute_paths=False):
-        stack = [(None, None)] # initialised with the sentinel
+        stack = [(None, None, 0)] # initialised with the sentinel
         rows = [] # accumulate the results row by row
         svx_path = [] # list of elements extracted from begin...end statements
         p = Path(svx_file).with_suffix('.svx') # add the suffix if not already present and work with absolute paths
         if absolute_paths:
             p = p.absolute()
         wd = p.parent # the working directory as an absolute path
-        fp, encoding = open_with_character_encoding(p, trace=trace)
+        encoding = character_encoding(p)
+        fp = p.open('r', encoding=encoding)
+        if trace:
+            print(f'Entering {p} ({encoding})')
         while fp: # will finish when the sentinel is encountered
             line = fp.readline() # read the first line
-            line_number = 1
-            while line:
+            line_number = 1 # and keep track of the line numbers
+            while line: # loop until we run out of lines
                 line = line.strip() # remove leading and trailing whitespace then remove comments
                 clean = line.split(self.comment_char)[0].strip() if self.comment_char in line else line
                 star_command = extract_star_command(clean, self.star_commands)
                 if star_command: # rejected if the none found
                     argument = clean.removeprefix(star_command).strip() # again strip whitespace
-                    row = (p, line_number, '.'.join(svx_path), star_command.removeprefix('*').upper(), argument, line.expandtabs())
-                    if line.lower().startswith('*begin'): # process a begin statement (use lower case)
+                    row = (p, line_number, encoding.upper(), '.'.join(svx_path),
+                           star_command.removeprefix('*').upper(), argument, line.expandtabs())
+                    if line.lower().startswith('*begin'): # process a begin statement (force lower case)
                         print('>> BEGIN encountered', p, line_number, '.'.join(svx_path), line)
-                        svx_path.append(argument.lower()) # use lower case here
+                        svx_path.append(argument.lower()) # force lower case here
                         begin_line_number, begin_line = line_number, line # keep a copy for debugging errors
                     if line.lower().startswith('*end'): # process an end statement
                         print('>> END encountered', p, line_number, '.'.join(svx_path), line)
@@ -97,17 +98,20 @@ class Analyzer:
                             print(f'BEGIN statement line {begin_line_number} in {p}: {begin_line}')
                             print(f'END statement line {line_number} in {p}: {line}')
                             raise Exception('mismatched begin...end statements')
-                    rows.append(row) # add to the growing accumulated data, then ..
-                    if line.lower().startswith('*include'): # .. process an include statement
-                        stack.append((p, fp)) # push the current file path and pointer onto stack
+                    rows.append(row) # add to the growing accumulated data
+                    if line.lower().startswith('*include'): # process an include statement
+                        stack.append((p, fp, line_number)) # push the current file path, pointer, and line number onto stack
                         filename = argument.strip('"').replace('\\', '/') # remove quotes and replace backslashes
                         wd = p.parent # the new working directory
-                        p = Path(wd, filename).with_suffix('.svx') # add the suffix if not already present
-                        fp, encoding = open_with_character_encoding(p, trace=trace)
+                        p = Path(wd, filename).with_suffix('.svx') # the new path (add the suffix if not already present)
+                        encoding = character_encoding(p)
+                        fp = p.open('r', encoding=encoding)
+                        if trace:
+                            print(f'Entering {p} ({encoding})')
                 line = fp.readline() # read the next line if there is one
                 line_number = line_number + 1 # and increment the line counter
             fp.close() # we ran out of lines for the file being currently processed
-            p, fp = stack.pop() # return to the including file (this always works, because of the sentinel)
+            p, fp, line_number = stack.pop() # back to the including file (this pop always returns, because of the sentinel)
 
         return pd.DataFrame(rows, columns=self.schema.keys()).astype(self.schema)
 
