2f23ef84fc9b7fcdc089ca497226218b7467a2bf
Patrick Warren <patrickbwarren@gmail.com>
2023-10-11 20:48:22 +0100

    prep for release

--- survex_analyzer.py
+++ survex_analyzer.py
@@ -44,15 +44,24 @@ def character_encoding(p):
         raise UnicodeDecodeError(f'Couldnt determine the character encoding for {p}')
     return encoding
 
-def extract_keyword(clean, keywords):
-    '''Extract a keyword from a cleaned up line, returning None if not present'''
-    keyword_list = [keyword for keyword in keywords if clean.lower().startswith(keyword)]
-    if '*cs out' in keyword_list: # special treatment because of the space
-        return ' '.join(clean.split()[:2]) # join the first two entries with a space (preserve case)
-    elif keyword_list:
-        return clean.split()[0] # this preserves case
-    else:
-        return None
+def extract_keyword_arguments(clean, keywords, keyword_char):
+    '''Extract a keyword and arguments from a cleaned up line, returning None if not present'''
+    if clean and clean[0] == keyword_char: # detect keyword by presence of keyword character
+        clean_list = clean[1:].split() # drop the keyword char and split on white space
+        if clean_list[0].lower() == 'cs' and clean_list[1].lower() == 'out': # special treatment for 'cs out'
+            keyword = ' '.join(clean_list[:2]) # join the first two entries with a space (preserve case)
+            arguments = clean_list[2:] # the remainder is the argument
+        else:
+            for keyword in keywords: # identify the keyword from the list of possible ones
+                if clean_list[0].lower() == keyword:
+                    keyword = clean_list[0] # the first entry, preserving case
+                    arguments = clean_list[1:] # the rest is the argument
+                    break
+            else: # terminal clause of for loop -- the keyword is not in the provided list
+                keyword, arguments = None, []
+    else: # the cleaned up line did not start with a keyword character
+        keyword, arguments = None, []
+    return keyword, arguments
 
 class Analyzer:
 
@@ -60,29 +69,32 @@ class Analyzer:
     # keywords being tracked.  The schema should be left alone unless
     # matching changes are made to the 'row =' line below.
     
-    def __init__(self, use_extra=False, comment_char=';',
-                 keywords=['*include', '*begin', '*end', '*fix', '*entrance', '*equate', '*cs out', '*cs'],
-                 extra_keywords=['*export', '*date', '*flags']):
+    def __init__(self, use_extra=False, comment_char=';', keyword_char='*',
+                 keywords=['include', 'begin', 'end',
+                           'fix', 'entrance', 'equate', 'cs out', 'cs'],
+                 extra_keywords=['export', 'date', 'flags']):
         self.keywords = (keywords + extra_keywords) if use_extra else keywords
         self.comment_char = comment_char
-        self.schema = {'file':str, 'encoding':str, 'line':int, 'survex_path':str, 'keyword':str, 'argument':str, 'full':str}
+        self.keyword_char = keyword_char
+        self.schema = {'file':str, 'encoding':str, 'line':int, 'survex_path':str,
+                       'keyword':str, 'argument':str, 'full':str}
 
     # Use a stack to keep track of the include files - items on the
     # stack are tuples of file paths and open file pointers.  The
-    # initial entry (None, None, ...) is used as a sentinel to stop
+    # initial entry (None, None, ...) acts as a sentinel to stop
     # the iteration.
 
     def analyze(self, svx_file, trace=False, absolute_paths=False):
-        if '*include' not in self.keywords: # this should always be present
-            self.keywords.insert(0, '*include') # as the first element
-        stack = [(None, None, 0, '')] # initialised with the sentinel
+        if 'include' not in self.keywords: # this should always be present
+            self.keywords.insert(0, 'include') # as the first element
+        stack = [(None, None, 0, '')] # initialised with a sentinel
         rows = [] # accumulate the results row by row
         svx_path = [] # list of elements extracted from begin...end statements
         p = Path(svx_file).with_suffix('.svx') # add the suffix if not already present and work with absolute paths
         if absolute_paths:
             p = p.absolute()
-        wd = p.parent # the working directory as an absolute path
-        encoding = character_encoding(p)
+        wd = p.parent # the working directory
+        encoding = character_encoding(p) # figure out the character encoding for the top level file
         fp = p.open('r', encoding=encoding)
         if trace:
             print(f'Entering {p} ({encoding})')
@@ -93,30 +105,29 @@ class Analyzer:
             while line: # loop until we run out of lines
                 line = line.strip() # remove leading and trailing whitespace then remove comments
                 clean = line.split(self.comment_char)[0].strip() if self.comment_char in line else line
-                keyword = extract_keyword(clean, self.keywords)
+                keyword, arguments = extract_keyword_arguments(clean, self.keywords, self.keyword_char) # preserving case
                 if keyword: # rejected if none found
-                    argument = clean.removeprefix(keyword).strip() # again strip whitespace
                     row = (p, encoding.upper(), line_number, '.'.join(svx_path),
-                           keyword.removeprefix('*').upper(), argument.expandtabs(), line.expandtabs())
-                    if line.lower().startswith('*begin'): # process a begin statement (force lower case)
-                        svx_path.append(argument.lower()) # force lower case here
+                           keyword.upper(), ' '.join(arguments), line.expandtabs())
+                    if keyword.lower() == 'begin' and arguments: # process a begin statement (force lower case)
+                        svx_path.append(arguments[0].lower()) # force lower case here (may be fixed in subsequent versions)
                         begin_line_number, begin_line = line_number, line # keep a copy for debugging errors
-                    if line.lower().startswith('*end'): # process an end statement
+                    if keyword.lower() == 'end' and arguments: # process an end statement
                         previous_argument = svx_path.pop()
-                        if previous_argument != argument.lower():
+                        if previous_argument != arguments[0].lower():
                             print(f'BEGIN statement line {begin_line_number} in {p}: {begin_line}')
                             print(f'END statement line {line_number} in {p}: {line}')
                             raise Exception('mismatched begin...end statements')
                     rows.append(row) # add to the growing accumulated data
-                    if line.lower().startswith('*include'): # process an include statement
+                    if keyword.lower() == 'include': # process an include statement
                         stack.append((p, fp, line_number,encoding)) # push the current path, pointer, line number and encoding onto stack
-                        filename = argument.strip('"').replace('\\', '/') # remove quotes and replace backslashes
+                        filename = ' '.join(arguments).strip('"').replace('\\', '/') # remove quotes and replace backslashes
                         wd = p.parent # the new working directory
                         p = Path(wd, filename).with_suffix('.svx') # the new path (add the suffix if not already present)
                         encoding = character_encoding(p)
                         fp = p.open('r', encoding=encoding)
                         if trace:
-                            print(f'Entering {p} ({encoding})')
+                            print(f'Visiting {p} ({encoding})')
                         line_number = 0 # reset the line number counter
                 line = fp.readline() # read the next line if there is one
                 line_number = line_number + 1 # and increment the line counter
